cdc04cbd4b61d4fa7992e9c04a523ee698fe6d86
hostalerye <hostalerye@gmail.com>
2012-03-28 11:54:59 +0200

    adding project

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var db = require('./config/dbconfig');
 
-/**
- * Module dependencies.
- */
+var Schema = mongoose.Schema;
 
-var express = require('express')
-  , routes = require('./routes');
+var TaskSchema = new Schema({
+  title    : String,
+	due_date : Date,
+	done : {type:Boolean, default:false}
+});
 
-var app = module.exports = express.createServer();
+mongoose.connect('mongodb://'+db.user+':'+db.pass+'@'+db.host+':'+db.port+'/'+db.name);
 
-// Configuration
+mongoose.model('Task', TaskSchema);
+
+var Task = mongoose.model('Task');
+
+var app = express.createServer();
 
 app.configure(function(){
+	app.use(express.logger());
+	app.use(express.errorHandler());
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/static'));
+	
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.listen('4000');
+
+
+app.post('/', function(req, res) {
+	var task = new Task(req.body.task);
+	task.save(function() {
+		console.log(task.title);
+		console.log(task.due_date);
+		console.log(task.done);
+		res.redirect('/');
+	});
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.get('/done/:id', function(req, res) {
+	Task.findById(req.params.id, function(err, task) {
+		if (!task)
+			return next(new Error('Could not find task'));
+		else {
+			task.done = true;
+			task.save(function(err) {
+				if (err)
+					console.log('error')
+				else
+					res.redirect('/')
+			});
+		}
+	});
 });
 
-// Routes
+app.get('/remove/:id', function(req, res) {
+	Task.remove({_id:req.params.id}, function() {
+		res.redirect('/')
+	});
+});
 
-app.get('/', routes.index);
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/', function(req, res) {
+	tasks =Task.find({});
+	tasks.sort('due_date', 1);
+	tasks.exec(function(err, tasks) {
+		res.render('tasks', {locals : {
+			tasks: tasks
+		}});	
+	})
+});
---
+++ config/dbconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+	user: "toto",
+	pass: "toto",
+	host: "flame.mongohq.com",
+	port: 27082,
+	name: "TODO"
+}
