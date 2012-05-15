231209c69b9d54e1893741a63c921bbdc3ebe4a8
hostalerye <hostalerye@gmail.com>
2012-05-15 10:33:28 +0200

    test failed

--- test/test.js
+++ test/test.js
@@ -2,6 +2,14 @@ var should = require("should");
 
 describe("Hello", function() {
     it("World", function() {
-        should.exist(false);
+        should.exist(true);
     });
 });
+
+describe('Array', function(){
+    describe('#indexOf()', function(){
+        it('should return -1 when the value is not present', function(){
+            [1,2,3].indexOf(1).should.equal(-1);
+        })
+    })
+})
