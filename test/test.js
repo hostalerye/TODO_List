920ea894e072484abdeb0ef2031854f1d841e8a3
hostalerye <hostalerye@gmail.com>
2012-06-14 11:30:53 +0200

    Test success

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe("Hello", function() {
 describe('Array', function(){
     describe('#indexOf()', function(){
         it('should return -1 when the value is not present', function(){
-            [1,2,3].indexOf(1).should.equal(-1);
+            [1,2,3].indexOf(4).should.equal(-1);
         })
     })
 })
