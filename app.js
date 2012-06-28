c208313c8e3b531a9b9e24c50be46f86e2c7235a
hostalerye <hostalerye@gmail.com>
2012-06-28 17:35:52 +0200

    Forth test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var db = require('./config/dbconfig');
 var Schema = mongoose.Schema;
-//Just to test commit hook bis
+//Just to test commit hook bis ter
 var TaskSchema = new Schema({
   title    : String,
 	due_date : Date,
